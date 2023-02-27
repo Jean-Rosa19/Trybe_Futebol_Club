2cf2153ee4ff6465ca41b42b0b9ce0e75accadc2
jean-rosa <jeancarlosrosa22@gmail.com>
2023-02-27 19:54:41 -0300

    criando a model teams

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class TeamsModel extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamsModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'teams',
+});
+
+export default TeamsModel;
