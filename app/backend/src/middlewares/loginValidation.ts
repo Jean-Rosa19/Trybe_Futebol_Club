e4725ceb4dd3e3d7468f529b65a12ca8be2a66bc
jean-rosa <jeancarlosrosa22@gmail.com>
2023-03-03 12:10:48 -0300

    resolução do requisito 10

--- app/backend/src/middlewares/loginValidation.ts
+++ app/backend/src/middlewares/loginValidation.ts
@@ -1,18 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
-const fieldsValidate = (req: Request, res: Response, next: NextFunction) => {
+const validateEmail = (email: string): boolean => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+
+function fieldsValidate(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
-  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  const isValidPassword = password.length >= 6;
 
-  if (!isValidEmail || !isValidPassword) {
+  if (!validateEmail(email) || password.length < 6) {
     return res.status(401).json({ message: 'Invalid email or password' });
   }
 
-  next();
-};
+  return next();
+}
 
 export default fieldsValidate;
--- app/backend/src/middlewares/validatetoken.ts
+++ app/backend/src/middlewares/validatetoken.ts
@@ -1,11 +1,11 @@
 import * as jwt from 'jsonwebtoken';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 import Iuser from '../interface/Iuser';
 
 const { JWT_SECRET } = process.env;
 
-export default function verifyToken(req: Request, res: Response) {
+export default function verifyToken(req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -15,9 +15,10 @@ export default function verifyToken(req: Request, res: Response) {
   try {
     const result = jwt.verify(authorization, JWT_SECRET as jwt.Secret);
     const { role } = result as Iuser;
-    return res.status(200).json({ role });
+    if (role) next();
   } catch ({ message }) {
     console.error(message);
     res.status(401).json({ message: 'Token must be a valid token' });
   }
+  next();
 }
