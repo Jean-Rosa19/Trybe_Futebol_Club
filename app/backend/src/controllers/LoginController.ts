12ab20931a6e4ee953dd7cd7ddfa4e83376a631e
jean-rosa <jeancarlosrosa22@gmail.com>
2023-03-02 21:09:19 -0300

    resolução do requisito 8 e teste da loginController

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -14,23 +14,3 @@ export default class UserController {
     }
   }
 }
-
-// import LoginService from '../services/LoginService';
-// import { createToken } from '../utils/jwt';
-
-// class LoginController {
-//   static async checkLogin(req: Request, res: Response) {
-//     const { email, password } = req.body;
-//     const checkLogin = await LoginService.login(email, password);
-
-//     if (!checkLogin) {
-//       return res.status(401).json({ message: 'Invalid email or password' });
-//     }
-//     const token = createToken({
-//       email: checkLogin,
-//     });
-//     res.status(200).json({ token });
-//   }
-// }
-
-// export default LoginController;
